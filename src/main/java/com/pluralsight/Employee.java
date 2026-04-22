a50ef418ee3c5ab02a6d0f8b6193a2b403a72c13
24rohitsamp <24rohitsamp@gmail.com>
2026-04-22 01:57:26 -0400

    Update Employee.java

--- src/main/java/com/pluralsight/Employee.java
+++ src/main/java/com/pluralsight/Employee.java
@@ -1,4 +1,35 @@
 package com.pluralsight;
 
 public class Employee {
+    private int employeeId;
+    private String name;
+    private double payRate;
+    private double hoursWorked;
+
+    //constructor
+    public Employee(int employeeId, String name, double hoursWorked, double payRate) {
+        this.employeeId = employeeId;
+        this.payRate = payRate;
+        this.name = name;
+        this.hoursWorked = hoursWorked;
+    }
+//Getters
+    public int getEmployeeId() {
+        return employeeId;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public double getHoursWorked() {
+        return hoursWorked;
+    }
+    public double getPayRate() {
+        return payRate;
+    }
+
+    public double getGrossPay() {
+        return hoursWorked * payRate;
+    }
 }
